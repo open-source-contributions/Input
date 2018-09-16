8310d654f6ac84b1c64f5d0f2c4a0a5012ebd6d2
Greg Bowler <greg.bowler@g105b.com>
2018-09-16 19:05:00 +0100

    Add example test file for dev-security

---
+++ test/unit/Security/FormFieldProtectorTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Gt\Input\Test\Security;
+
+use PHPUnit\Framework\TestCase;
+
+class FormFieldProtectorTest extends TestCase{
+	public function testExample() {
+		self::assertTrue(true);
+	}
+}
